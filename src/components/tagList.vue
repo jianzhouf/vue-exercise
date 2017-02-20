980877b87ec09e10bc2451b5f1aec38aa6d72134
wb-zhoujian <wb-zhoujian@alibaba-inc.com>
2017-02-20 16:34:54 +0800

    router-link跳转；created 修改 title；keyup.enter

--- src/components/tagList.vue
+++ src/components/tagList.vue
@@ -1,9 +1,9 @@
 <template>
   <div >
-    <input type="text" v-model="newItem" placeholder="请输入内容">
+    <input type="text" v-model="newItem" placeholder="请输入内容  " @keyup.enter="addItem">
     <button @click="addItem">ADD</button>
     <transition-group name="tabItem" tag="ul">
-        <li v-for="(item, index) in list"  :key="index">{{item}}&nbsp;<button @click="removeItem(index)">X</button></li>
+        <li class="list-item" v-for="(item, index) in list"  :key="index">{{item}}&nbsp;<button @click="removeItem(index)">X</button></li>
     </transition-group>
     
   </div>
@@ -12,6 +12,9 @@
 <script>
 export default {
   name: 'tagList',
+  created () {
+    document.title = 'tagList'
+  },
   data () {
     return {
       list: [],
@@ -20,8 +23,10 @@ export default {
   },
   methods: {
     addItem: function () {
-      this.list.push(this.newItem)
-      this.newItem = ''
+      if (this.newItem.trim()) {
+        this.list.push(this.newItem)
+        this.newItem = ''
+      }
     },
     removeItem: function (i) {
       this.list.splice(i, 1)
@@ -36,10 +41,23 @@ export default {
     list-style-type: none;
     padding: 0;
   }
+  .list-item{
+    margin-bottom: 10px; 
+    overflow: hidden;
+    width: 500px;
+    margin-left: 400px;
+    background-color: red;
+    color: white;
+    text-align: left;
+  }
+  .list-item button{
+    float: right;
+  }
   .tabItem-enter-active, .tabItem-leave-active {
-    transition: opacity 1s
+    transition: all 1s;
   }
   .tabItem-enter, .tabItem-leave-active {
-    opacity: 0
+    opacity: 0;
+    transform: translate(50px);
   } 
 </style>
